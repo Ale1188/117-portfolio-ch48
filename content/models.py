7aa3b1eea7fb9d34919d97852c8120d23c97463d
Ale1188 <dramoizta@gmail.com>
2024-09-24 20:12:42 -0700

    Class 2 - assignment 1 finish

--- content/models.py
+++ content/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class Skill(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
 class Project(models.Model):
@@ -11,3 +14,6 @@ class Project(models.Model):
     image = models.ImageField(upload_to='static/img')
     repository = models.URLField()
     skills = models.ManyToManyField(Skill)
+
+    def __str__(self):
+        return f"{self.name} - ({self.year})"
--- content/urls.py
+++ content/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import project_list, experience_list
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('', project_list, name='projects_list'),
     path('experience_list', experience_list, name='experience_list'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
